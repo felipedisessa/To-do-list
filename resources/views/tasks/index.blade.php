dbf0379971d92e83bcc66beb840cb447c3a66476
Felipe Di Sessa <felipetmds@hotmail.com>
2024-10-22 16:52:29 -0300

    create TaskController

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-slate-800 dark:text-slate-200 leading-tight">
+            {{ __('Lista de Tarefas') }}
+        </h2>
+    </x-slot>
+</x-app-layout>
